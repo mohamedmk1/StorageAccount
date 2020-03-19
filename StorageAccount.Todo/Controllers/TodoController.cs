98d8d462e2113d74252a37012a7eb0e86009d419
m.el-mabrouk <mohamed.el-mabrouk-extern@renault.com>
2020-03-19 13:08:20 +0100

    create api crud operations with storage account

---
+++ StorageAccount.Todo/Controllers/TodoController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pluralight.Todo.Models;
+using Pluralight.Todo.Repositories;
+using static Pluralight.Todo.Repositories.TodoRepository;
+
+namespace Pluralight.Todo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+
+        private readonly ILogger<TodoController> _logger;
+        private readonly ITodoRepository _repository;
+
+
+        public TodoController(ILogger<TodoController> logger, ITodoRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoModel> Get()
+        {
+            var entities = _repository.GetAllTodo();
+            var models = entities.Select(x => new TodoModel
+            {
+                Id = x.RowKey,
+                Group = x.PartitionKey,
+                Content = x.Content,
+                Due = x.Due,
+                Completed = x.Completed,
+                Timestamp = x.Timestamp
+            });
+
+            return models;
+        }
+
+        [HttpGet("{Group}/{Id}")]
+        public ActionResult<TodoModel> GetTodoItem(string group, string id)
+        {
+            var result = _repository.GetTodoById(group, id);
+
+            return new TodoModel {
+                Id = id,
+                Group = group,
+                Content = result.Content,
+                Completed = result.Completed,
+                Due = result.Due
+            };
+        }
+
+        [HttpPost("{Group}")]
+        public ActionResult<TodoModel> CreateTodoItem(TodoModel item, string group)
+        {
+            var entity = new TodoEntity
+            {
+                PartitionKey = group,
+                RowKey = Guid.NewGuid().ToString(),
+                Completed = item.Completed,
+                Content = item.Content,
+                Due = item.Due
+            };
+
+            _repository.CreateTodoItem(entity);
+
+            item.Id = entity.RowKey;
+            item.Group = entity.PartitionKey;
+
+            return CreatedAtAction(nameof(GetTodoItem), new { group = entity.PartitionKey,id = entity.RowKey }, item);
+        }
+
+        [HttpPut("{Group}/{Id}")]
+        public void Update(TodoModel model, string group, string id)
+        {
+            _repository.UpdateToDoIntem(new TodoEntity
+            {
+                PartitionKey = group,
+                RowKey = id,
+                Completed = model.Completed,
+                Content = model.Content,
+                Due = model.Due
+            });
+        }
+
+        [HttpDelete("{Group}/{Id}")]
+        public ActionResult<TodoModel> Delete(string group, string id)
+        {
+            var item = _repository.GetTodoById(group, id);
+
+            _repository.DeleteTodoItem(item);
+            return new TodoModel
+            {
+                Id = id,
+                Group = group,
+                Content = item.Content,
+                Completed = item.Completed,
+                Due = item.Due
+            };
+        }
+    }
+}
---
+++ StorageAccount.Todo/Repositories/TodoRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Cosmos.Table;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pluralight.Todo.Repositories
+{
+    public interface ITodoRepository
+    {
+        IEnumerable<TodoEntity> GetAllTodo();
+        void CreateTodoItem(TodoEntity item);
+        void UpdateToDoIntem(TodoEntity item);
+        void DeleteTodoItem(TodoEntity item);
+        TodoEntity GetTodoById(string group, string id);
+    }
+
+    public class TodoRepository: ITodoRepository
+    {
+
+        private CloudTable todoTable = null;
+        public TodoRepository()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storageacpluralisght;AccountKey=FoIktP9sACEGjoGhdR7mDn4nGjRMGyAapcTl52VSpYb3NGQtfYyKxTEMcaAmhJQvVx9065rC9ICAXxjkJQHE3Q==;EndpointSuffix=core.windows.net");
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            todoTable = tableClient.GetTableReference("Todo");
+        }
+
+        public IEnumerable<TodoEntity> GetAllTodo()
+        {
+            var query = new TableQuery<TodoEntity>();
+
+            var entities = todoTable.ExecuteQuery(query);
+
+            return entities;
+        }
+
+        public void CreateTodoItem(TodoEntity entity)
+        {
+            var operation = TableOperation.Insert(entity);
+
+            todoTable.Execute(operation);
+        }
+
+        public void UpdateToDoIntem(TodoEntity entity)
+        {
+            var operation = TableOperation.InsertOrReplace(entity);
+
+            todoTable.Execute(operation);
+        }
+
+        public void DeleteTodoItem(TodoEntity entity)
+        {
+            var operation = TableOperation.Delete(entity);
+
+            todoTable.Execute(operation);
+        }
+
+        public  TodoEntity GetTodoById(string partitionKey, string rowKey)
+        {
+            var operation = TableOperation.Retrieve<TodoEntity>(partitionKey, rowKey);
+
+            var result = todoTable.Execute(operation);
+
+            return result.Result as TodoEntity;
+        }
+    }
+
+    public class TodoEntity : TableEntity
+    {
+        public string Content { get; set; }
+        public bool Completed { get; set; }
+        public string Due { get; set; }
+    }
+}
